981f89e4f96ea62a7a3a6156337d0bd6cbf49013
Danny Festor <danny.festor@gmail.com>
2023-01-06 08:58:58 +0900

    Test Updating Post

--- app/Http/Livewire/Admin/Post/Edit.php
+++ app/Http/Livewire/Admin/Post/Edit.php
@@ -2,12 +2,51 @@
 
 namespace App\Http\Livewire\Admin\Post;
 
+use App\Models\Post;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    public Post $post;
+
+    public string $title = '';
+    public string $released_at = '';
+    public string $slug = '';
+    public string $description = '';
+    public bool $is_released = false;
+
+    public function getRules(): array
+    {
+        return [
+            'title' => ['required', 'min:10', 'max:255',],
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($this->post->id), 'min:10', 'max:100'],
+            'description' => ['required'],
+            'released_at' => ['nullable', 'date'],
+            'is_released' => ['required', 'boolean'],
+        ];
+    }
+
+    public function mount()
+    {
+        $this->title = $this->post->title;
+        $this->released_at = $this->post->released_at;
+        $this->slug = $this->post->slug;
+        $this->description = $this->post->description;
+        $this->is_released = $this->post->is_released;
+    }
+
     public function render()
     {
         return view('livewire.admin.post.edit');
+    }
+
+    public function onSubmit()
+    {
+        $validated = $this->validate();
+
+        Post::query()
+            ->where('id', '=', $this->post->id)
+            ->update($validated);
     }
 }
