154c5a85b72d7a9038e37b4669b2b85c7bdb842e
Danny Festor <danny.festor@gmail.com>
2023-01-06 16:21:11 +0900

    Update Designs, Delete Unused Pages

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,19 +1,10 @@
 <x-app-layout>
     <x-slot:title>{{ __('Blog')  }}</x-slot:title>
     <x-layouts.partials.content-wrap>
-        @if(Session::has('success'))
-            <section class="border-2 border-green-600 bg-green-200 text-green-800 px-4 py-2 max-w-4xl mx-auto rounded mb-4 text-center">
-                {{ Session::get('success') }}
-            </section>
-        @endif
-
         <article class="bg-white p-4 sm:p-8 rounded max-w-4xl mx-auto shadow">
             <h1 class="text-4xl font-bold font-serif">
                 {{ $post->title }}
             </h1>
-            @if(auth()->user()?->can('update', $post))
-                <a href="{{ route('admin.post.edit', $post) }}">Edit</a>
-            @endif
 
             <section class="flex justify-end">
                 {{ $post->user->name }}
