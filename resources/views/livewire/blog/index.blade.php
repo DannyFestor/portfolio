de4fbe2fc2dc6ba71eb8e897ffa35988c9face80
Danny Festor <danny.festor@gmail.com>
2023-01-10 17:20:55 +0900

    Show Feed Link

--- resources/views/livewire/blog/index.blade.php
+++ resources/views/livewire/blog/index.blade.php
@@ -19,6 +19,17 @@
             }" class="grid sm:grid-cols-4 gap-4 max-w-7xl mx-auto">
         <form @submit.prevent
               class="flex flex-col bg-white shadow rounded p-4 sm:col-span-3">
+
+            <section class="mb-2">
+                <a href="{{ route('blog.feed') }}" class="flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 19.5v-.75a7.5 7.5 0 00-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>
+
+                    RSS Feed <small>(filter posts with <em>?tag=Laravel</em>, Pagination with ?page=2)</small>
+                </a>
+            </section>
+
             <label class="flex items-center gap-4">
                 Search:
                 <input type="text"
