512e01e8304b9ba547066fed08c99c43354cc8c8
Danny Festor <danny@festor.info>
2023-03-09 18:01:13 +0900

    Add CommonMark/Shiki

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Models\Tag;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
+use League\CommonMark\Extension\TableOfContents\TableOfContentsExtension;
+use League\CommonMark\MarkdownConverter;
+use League\CommonMark\Util\HtmlFilter;
 
 class PostController
 {
@@ -19,12 +24,55 @@ class PostController
     {
         $user = \Auth::user();
 
-        if ((!$user || !$user->is_admin) && (!$post->is_released || ($post->released_at && $post->released_at->gt(now())))) {
+        if ((!$user || !$user->is_admin) && (!$post->is_released || ($post->released_at && $post->released_at->gt(
+                        now()
+                    )))) {
             abort(404);
         }
 
         $post->load(['tags']);
         $post->getFirstMedia(Post::HERO_IMAGE);
+
+        $config = [
+            'html_input' => 'allow',
+//            'html_input' => 'escape',
+            'table_of_contents' => [
+                'html_class' => 'table-of-contents',
+                'position' => 'top',
+                'style' => 'bullet',
+                'min_heading_level' => 1,
+                'max_heading_level' => 6,
+                'normalize' => 'relative',
+                'placeholder' => null,
+            ],
+            'heading_permalink' => [
+                'html_class' => 'heading-permalink',
+                'id_prefix' => 'content',
+                'fragment_prefix' => 'content',
+                'insert' => 'before',
+                'min_heading_level' => 1,
+                'max_heading_level' => 6,
+                'title' => 'Permalink',
+                'symbol' => '#',
+                'aria_hidden' => true,
+            ],
+        ];
+//
+//// Configure the Environment with all the CommonMark parsers/renderers
+        $environment = new Environment($config);
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new \League\CommonMark\Extension\Table\TableExtension());
+        $environment->addExtension(new \League\CommonMark\Extension\Strikethrough\StrikethroughExtension());
+        $environment->addExtension(new \League\CommonMark\Extension\Footnote\FootnoteExtension());
+        $environment->addExtension(new \League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension());
+        $environment->addExtension(new \League\CommonMark\Extension\TableOfContents\TableOfContentsExtension());
+
+// Instantiate the converter engine and start converting some Markdown!
+        $converter = new MarkdownConverter($environment);
+//        dd($post->description);
+
+        $post->description = $converter->convert($post->description)->getContent();
+//        dd($post->description);
 
         return view('blog.show', ['post' => $post]);
     }
@@ -33,11 +81,11 @@ class PostController
     {
         $posts = Post::query()
             ->with('user')
-            ->where('is_released', TRUE)
+            ->where('is_released', true)
             ->whereNotNull('released_at')
             ->where('released_at', '<=', now())
             ->when($request->get('tag'), function (Builder $query, $tag) {
-                if(is_array($tag)) {
+                if (is_array($tag)) {
                     $tag = $tag[0];
                 }
                 $tag = strtolower($tag);
@@ -49,7 +97,12 @@ class PostController
             ->orderBy('released_at', 'DESC')
             ->paginate(30);
 
-        $posts->each(fn (Post $post) => $post->description = app(\Spatie\LaravelMarkdown\MarkdownRenderer::class)->toHtml(\Str::limit($post->description), 300));
+        $posts->each(
+            fn(Post $post) => $post->description = app(\Spatie\LaravelMarkdown\MarkdownRenderer::class)->toHtml(
+                \Str::limit($post->description),
+                300
+            )
+        );
 
         return response()->view('blog.feed', ['posts' => $posts])->header('Content-Type', 'application/xml');
     }
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -18,25 +18,47 @@
             <section class="flex justify-end p-4 sm:p-8">
                 {{ $post->user->name }}
                 @if($post->released_at)
-                -
-                {{ $post->released_at->diffForHumans() }}
+                    -
+                    {{ $post->released_at->diffForHumans() }}
                 @endif
             </section>
 
             @if($post->tags->isNotEmpty())
-            <section class="flex flex-wrap gap-2 mb-4 p-4 sm:p-8">
-                @foreach($post->tags as $tag)
-                    <div class="px-2 py-1 rounded flex items-center gap-1" style="color: {{ $tag->text_color }}; background-color: {{ $tag->background_color }}; border: 1px solid {{ $tag->border_color }}">
-                        <img class="h-6 w-6" src="{{ asset('icons/' . $tag->logo ) }}" alt="">
-                        {{ $tag->title }}
-                    </div>
-                @endforeach
-            </section>
+                <section class="flex flex-wrap gap-2 mb-4 p-4 sm:p-8">
+                    @foreach($post->tags as $tag)
+                        <div class="px-2 py-1 rounded flex items-center gap-1"
+                             style="color: {{ $tag->text_color }}; background-color: {{ $tag->background_color }}; border: 1px solid {{ $tag->border_color }}">
+                            <img class="h-6 w-6" src="{{ asset('icons/' . $tag->logo ) }}" alt="">
+                            {{ $tag->title }}
+                        </div>
+                    @endforeach
+                </section>
             @endif
 
-            <x-markdown class="prose font-serif min-w-full p-4 sm:p-8">
+            <div class="prose prose:text-base prose-pre:bg-white prose-pre:text-base font-serif min-w-full p-4 sm:p-8" id="post">
                 {!! $post->description !!}
-            </x-markdown>
+            </div>
         </article>
+
+        <script defer src="https://cdn.jsdelivr.net/npm/shiki"></script>
+
+        <script defer>
+            document.addEventListener('DOMContentLoaded', () => {
+                // console.log('do it');
+                const elements = document.querySelectorAll("#post pre code");
+                elements.forEach((element) => {
+                    const language = element.className.match(/\s*language-(.*)\s*/)
+                    console.log(language, typeof language, language !== null ? language[1] : 0);
+                    shiki
+                        .getHighlighter({
+                            theme: 'one-dark-pro',
+                        })
+                        .then(highlighter => {
+                            const code = highlighter.codeToHtml(element.innerText, { lang: language !== null ? language[1] ?? "html" : "html" });
+                            element.innerHTML = code;
+                        });
+                })
+            });
+        </script>
     </x-layouts.partials.content-wrap>
 </x-app-layout>
