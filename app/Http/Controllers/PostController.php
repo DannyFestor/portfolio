f6f12a4cf28d717fe1f03c0fd47d0f1f22fcab8b
Danny Festor <danny@festor.info>
2023-03-09 19:39:10 +0900

    Update TOC Display

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,12 +38,14 @@ class PostController
 //            'html_input' => 'escape',
             'table_of_contents' => [
                 'html_class' => 'table-of-contents',
-                'position' => 'top',
+//                'position' => 'top',
+                'position' => 'placeholder',
+                'placeholder' => '[TOC]',
                 'style' => 'bullet',
                 'min_heading_level' => 1,
                 'max_heading_level' => 6,
                 'normalize' => 'relative',
-                'placeholder' => null,
+//                'placeholder' => null,
             ],
             'heading_permalink' => [
                 'html_class' => 'heading-permalink',
