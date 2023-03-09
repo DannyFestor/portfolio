4dcc651e5c6234b0e5265a5bf2121064589c0d1f
Danny Festor <danny@festor.info>
2023-03-09 18:58:49 +0900

    Remove console.log

--- resources/js/shiki.js
+++ resources/js/shiki.js
@@ -1,43 +1,40 @@
 import { getHighlighter, setCDN } from 'shiki';
 
-console.log('SHIKI');
 setCDN('/assets/shiki/');
 
 document.addEventListener('DOMContentLoaded', async () => {
     const elements = document.querySelectorAll('#post pre code');
-    console.log(elements);
     const highlighter = await getHighlighter({
         theme: 'one-dark-pro',
         langs: [
-            "css",
-            "dart",
-            "docker",
-            "go",
+            'css',
+            'dart',
+            'docker',
+            'go',
             'html',
-            "javascript",
+            'javascript',
             // "json",
             // "markdown",
-            "php",
+            'php',
             // "php-html",
-            "rust",
-            "sass",
-            "scss",
-            "shellscript",
-            "sql",
+            'rust',
+            'sass',
+            'scss',
+            'shellscript',
+            'sql',
             // "svelte",
-            "tsx",
-            "typescript",
+            'tsx',
+            'typescript',
             // "vue",
-            "vue-html",
+            'vue-html',
         ],
     });
 
     elements.forEach((element) => {
         const language = element.className.match(/\s*language-(.*)\s*/);
-        console.log(language, typeof language,
-            language !== null ? language[1] : 0);
-        const code = highlighter.codeToHtml(element.innerText,
-            { lang: language !== null ? language[1] ?? 'html' : 'html' });
-        element.innerHTML = code;
+        element.innerHTML = highlighter.codeToHtml(
+            element.innerText,
+            { lang: language !== null ? language[1] ?? 'html' : 'html' },
+        );
     });
 });
