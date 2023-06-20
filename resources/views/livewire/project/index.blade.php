3d519dcab7a2dbc24887e267ee8b38dd9a74fc29
Danny Festor <danny@festor.info>
2023-06-21 08:49:55 +0900

    Add URLS

--- resources/views/livewire/project/index.blade.php
+++ resources/views/livewire/project/index.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.partials.content-wrap>
     <section class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-8">
         @foreach($projects as $project)
-            <article class="rounded-lg p-4 bg-white shadow-sm transition-all hover:scale-105 hover:shadow max-w-md">
+            <article class="rounded-lg p-4 bg-white shadow-sm transition-all hover:scale-105 hover:shadow max-w-md flex flex-col">
                 <h2 class="font-bold text-center">
                     {{ $project->title }}
                 </h2>
@@ -13,6 +13,12 @@
                 <p class="prose">
                     {{ $project->body }}
                 </p>
+                @if($project->git_url)
+                    <a href="{{ $project->git_url }}">Github</a>
+                @endif
+                @if($project->live_url)
+                    <a href="{{ $project->live_url }}">Live</a>
+                @endif
             </article>
         @endforeach
     </section>
