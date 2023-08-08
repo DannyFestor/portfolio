cb38209a52041c4cb96d07a02a71a3dfe1484b19
Danny Festor <danny@festor.info>
2023-08-08 16:16:37 +0900

    Fix Wording

--- lang/de/homepage.php
+++ lang/de/homepage.php
@@ -4,49 +4,50 @@ return [
     'about' => <<<'HTML'
             <div class="prose" style="margin-bottom: 2rem">
                 <p>
-                   Hallo, ich bin Danny, ein deutscher, freiberuflicher Webentwickler, der in Nagasaki, Japan,
-                   mit meiner Frau und meiner Tochter lebt.
+                   Hallo, ich bin Danny, ein deutscher, freiberuflicher Webentwickler,
+                   der in Nagasaki, Japan, mit meiner Frau und meiner Tochter lebt.
                 </p>
                 <p>
-                    Ich habe viele Jahre Erfahrung mit PHP und Laravel bei der Umsetzung von die Traumprojekte
-                    meiner Kunden und helfe Ihnen, mit ihren Kunden Verbunden zu sein.
+                    Ich habe viele Jahre Erfahrung mit PHP und Laravel bei der Umsetzung von Traumprojekten
+                    meiner Kunden, und ich bin sicher ich kann Ihnen helfen, über die von mir entwickelten Webseiten,
+                    mit ihren Kunden verbunden zu bleiben zu können.
                 </p>
                 <p>
                     Meine Leidenschaft ist die Erstellung schneller, funktionaler, benutzerfreundlicher und
-                    reaktiver Websites mit PHP, Laravel und Livewire. Ich habe ein ausgeprägtes Verständnis
-                    für Best Practices in der Webentwicklung und bin immer auf dem neuesten Stand der Technik.
-                    </p>
+                    reaktiver Websites mittels moderner Technologien wie PHP, Laravel und Livewire.
+                    Ich habe ein ausgeprägtes Verständnis für Best Practices in der Webentwicklung
+                    und bin immer auf dem neuesten Stand der Technik.
+                </p>
                 <p>
-                    Ich bin auch ein großartiger Kommunikator und arbeite gerne mit Kunden zusammen, um ihre
-                    Bedürfnisse zu verstehen und eine Website zu erstellen, die ihren Erwartungen entspricht.
+                    Ich arbeite gerne mit Kunden zusammen und setze dabei auf starke Kommunikation,
+                    um Bedürfnisse zu verstehen und Websiten zu erstellen, die den Erwartungen entspricht.
                     In regelmäßigen Besprechungen biete ich meinen Kunden die Möglichkeit, den aktuellen
-                    Fortschritt zu überprüfen und die nächsten Schritte anzupassen, um sicherzustellen, dass
-                    sich das Projekt genau wie erhofft entwickelt.
+                    Fortschritt zu überprüfen, Feedback zu geben, und die nächsten Schritte anzupassen, um sicherzustellen,
+                    dass sich das Projekt genau wie erhofft entwickelt. Dieser Feedback-Loop ist mir besonders wichtig.
                 </p>
                 <p>
                     Hier sind einige der Dienstleistungen, die ich anbiete:
                     <ul>
-                        <li>Custom Website Development - Realisieren Sie Ihre Traum-Homepage</li>
+                        <li>Website Entwicklung - Realisieren Sie Ihre Traum-Homepage</li>
                         <li>Beratungsdienste/Consulting - Sie wissen nicht, was Sie wollen oder brauchen? Buchen Sie ein Beratungsgespräch mit mir, um Ihre Optionen zu besprechen</li>
-                        <li>E-Commerce-Entwicklung - Sie wollen Kunden erreichen und etwas verkaufen? Lassen Sie uns eine maßgeschneiderte Lösung erstellen, die genau Ihren Bedürfnissen entspricht, oder eine fertige Lösung verwenden, die auf die Bedürfnisse Ihres Projekts abgestimmt ist.</li>
+                        <li>E-Commerce-Entwicklung - Sie wollen Kunden erreichen und etwas verkaufen? Lassen Sie uns gemeinsam eine maßgeschneiderte Lösung erstellen, die genau Ihren Bedürfnissen entsprich.</li>
                         <li>Content-Management-Systeme (CMS) - Ich baue ein CMS, das genau Ihren Bedürfnissen entspricht; keine unsicheren Plugin-Monster nötig</li>
                         <li>SEO - Mit optimierten Meta-Tags und semantischem HTML an die Spitze der Google-Ergebnisse springen</li>
-                        <li>Wartung und Support - Erhalten Sie immer die neuesten Sicherheitsupdates und neuen Funktionen</li>
+                        <li>Wartung und Support - Erhalten Sie immer die neuesten Sicherheitsupdates und Funktionen</li>
                     </ul>
                 </p>
                 <p>
                     Ich bin zuversichtlich, dass ich Ihnen helfen kann, eine Website zu erstellen, die Ihren
-                    Anforderungen entspricht und Ihre Erwartungen übertrifft.
+                    Anforderungen entspricht und Ihre Erwartungen übertrifft!
                 </p>
                 <p>
                     Sie können einige meiner jüngsten Projekte sehen, indem Sie zur
-                    <a href="/projects">Projekt-Seite</a>
-                    navigieren.</p>
+                    <a href="/projects">Projekt-Seite</a> navigieren.</p>
                 <p>
                     Wenn Sie an einer Zusammenarbeit mit mir interessiert sind, setzen
                     Sie sich bitte über die <a href="/kontakt">Kontaktseite</a> mit mir in Verbindung.
                     Ich würde mich freuen, Ihr Projekt zu besprechen und zu sehen, wie ich Ihnen helfen kann,
-                    Ihre Ziele zu erreichen.
+                    Ihre Ziele zu erreichen und Träume zu verwirklichen.
                 </p>
             </div>
     HTML,
