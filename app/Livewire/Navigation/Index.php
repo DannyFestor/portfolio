5b0a0c1ce1ce4ab721105e95866287251a1b5c76
Danny Festor <danny@festor.info>
2023-08-08 16:55:51 +0900

    Revert Delete..........

---
+++ app/Livewire/Navigation/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Livewire\Navigation;
+
+use App\Enums\Locales;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public bool $isOpen = false;
+
+    public function render(): \Illuminate\Contracts\View\View
+    {
+        return view('livewire.navigation.index');
+    }
+
+    public function change(Request $request)
+    {
+        $referrer = $request->server('HTTP_REFERER');
+
+        $request->validate([
+            'route' => ['required', 'string'],
+            'locale' => [Rule::in(Locales::toArray())],
+        ]);
+        $request->session()->put('locale', $request->get('locale'));
+
+        return redirect()->to($request->get('route'));
+    }
+}
