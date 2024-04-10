3ba94366de596932eac3b5c32479616752e6697d
Danny Festor <danny.festor@gmail.com>
2024-04-10 17:39:15 +0900

    Readd PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Markdown;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PostController extends Controller
+{
+    public function rssFeed(Request $request): Response
+    {
+        $posts = Post::query()
+            ->with('user')
+            ->where('is_released', true)
+            ->whereNotNull('released_at')
+            ->where('released_at', '<=', now())
+            ->when($request->get('tag'), function (Builder $query, $tag) {
+                if (is_array($tag)) {
+                    $tag = $tag[0];
+                }
+                $tag = strtolower($tag);
+
+                $query->whereHas('tags', function (Builder $query) use ($tag) {
+                    $query->whereRaw('LOWER(tags.title) like ?', ["%$tag%"]);
+                });
+            })
+            ->orderBy('released_at', 'DESC')
+            ->paginate(30);
+
+        $posts->each(
+            fn (Post $post) => $post->description = Markdown::make(
+                \Str::limit($post->description, 300),
+            )
+        );
+
+        return response()->view('blog.feed', ['posts' => $posts])->header('Content-Type', 'application/xml');
+    }
+}
