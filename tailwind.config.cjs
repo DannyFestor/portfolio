d21a07e8a7e3753c6fb2e3a3301504dd24d3ce9a
Danny Festor <danny.festor@gmail.com>
2024-04-25 20:08:26 +0900

    Update Tailwind Config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,9 @@ import colors from 'tailwindcss/colors';
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
         './resources/views/**/*.js',
         './resources/js/**/*.js',
         './resources/views/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
     ],
     darkMode: 'class',
     theme: {
