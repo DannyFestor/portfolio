d17d6e84676ad5047ea627102634732588665f9f
Danny Festor <danny.festor@gmail.com>
2024-04-25 15:12:31 +0900

    Fix metatags

--- app/Livewire/Homepage.php
+++ app/Livewire/Homepage.php
@@ -15,7 +15,7 @@ class Homepage extends Component
 
     public function render(): \Illuminate\Contracts\View\View
     {
-        $metatags = $this->getMetatagsForLocale($this->locale ?? app()->getLocale());
+        $metatags = $this->getMetatags();
 
         return view(
             'livewire.homepage',
@@ -25,71 +25,38 @@ class Homepage extends Component
         )->title(__('homepage.title'));
     }
 
-    private function getMetatagsForLocale(string $locale): string
+    private function getMetatags(): string
     {
-        $imagePath = public_path('img/danny-640.jpeg');
+        /** @var string $path */
+        $path = config('app.url');
+        $imagePath = $path . public_path('img/danny-640.jpeg');
 
-        return match ($locale) {
-            'de' => <<<HTML
-<link rel="home" href="http://portfolio.test">
-<link rel="canonical" href="http://portfolio.test/{$locale}">
-<meta name="title" content="Moderne, sichere Webseiten und Services - festor.info">
-<meta name="keywords" content="Fullstack,Freelance,Webdev,Sicher,Modern,Schnell,PHP,Golang,Typescript,Vue,Deutsch,Englisch,Japanisch">
-<meta name="description" content="Moderne, sichere Webseiten und Services. Ich realisiere Ihr Traumprojekt schnell und zuverlässig. Gute Kommunikation ist mir wichtig. festor.info">
-<meta name="twitter:site" content="@Denakino">
+        $title = __('metatags.title');
+        $titleShort = __('metatags.title.short');
+        $keywords = __('metatags.keywords');
+        $twitterHandle = __('metatags.twitter.username');
+        $locale = __('metatags.locale');
+        $localeUrl = __('metatags.locale.url');
+
+        return <<<HTML
+<link rel="home" href="{$path}">
+<link rel="canonical" href="{$path}/{$localeUrl}">
+<meta name="title" content="{$title}">
+<meta name="keywords" content="{$keywords}">
+<meta name="description" content="{$title}">
+<meta name="twitter:site" content="{$twitterHandle}">
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:creator" content="@Denakino">
-<meta name="twitter:description" content="Moderne, sichere Webseiten und Services. Ich realisiere Ihr Traumprojekt schnell und zuverlässig. Gute Kommunikation ist mir wichtig. festor.info">
+<meta name="twitter:creator" content="{$twitterHandle}">
+<meta name="twitter:description" content="{$title}">
 <meta name="twitter:image" content="{$imagePath}">
-<meta name="twitter:title" content="Moderne, sichere Webseiten und Services - festor.info">
-<meta name="og:description" content="Moderne, sichere Webseiten und Services. Ich realisiere Ihr Traumprojekt schnell und zuverlässig. Gute Kommunikation ist mir wichtig. festor.info">
+<meta name="twitter:title" content="{$titleShort}">
+<meta name="og:description" content="{$title}">
 <meta name="og:image" content="{$imagePath}">
-<meta name="og:locale" content="de_DE">
+<meta name="og:locale" content="{$locale}">
 <meta name="og:site_name" content="festor.info">
-<meta name="og:title" content="Moderne, sichere Webseiten und Services - festor.info">
-<meta name="og:type" content="homepage">
-<meta name="og:url" content="http://portfolio.test/{$locale}">
-HTML,
-            'ja' => <<<'HTML'
-<link rel="home" href="http://portfolio.test">
-<link rel="canonical" href="http://portfolio.test/{$locale}">
-<meta name="title" content="モダンで安全なウェブサイトとサービス - festor.info">
-<meta name="keywords" content="フルスタック,Web開発,多言語対策,安全,モダーン,はやい,PHP,Go言語,Typescript,Vue,日本語,英語,ドイツ語">
-<meta name="description" content="モダンで安全なウェブサイトとサービス。あなたの夢のプロジェクトを迅速かつ確実に実現します。良いコミュニケーションは私にとって重要です。 - festor.info">
-<meta name="twitter:site" content="@Denakino">
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:creator" content="@Denakino">
-<meta name="twitter:description" content="モダンで安全なウェブサイトとサービス。あなたの夢のプロジェクトを迅速かつ確実に実現します。良いコミュニケーションは私にとって重要です。 - festor.info">
-<meta name="twitter:image" content="{$imagePath}">
-<meta name="twitter:title" content="モダンで安全なウェブサイトとサービス - festor.info">
-<meta name="og:description" content="モダンで安全なウェブサイトとサービス。あなたの夢のプロジェクトを迅速かつ確実に実現します。良いコミュニケーションは私にとって重要です。 - festor.info">
-<meta name="og:image" content="{$imagePath}">
-<meta name="og:locale" content="ja_JP">
-<meta name="og:site_name" content="festor.info">
-<meta name="og:title" content="モダンで安全なウェブサイトとサービス - festor.info">
-<meta name="og:type" content="homepage">
-<meta name="og:url" content="http://portfolio.test/{$locale}">
-HTML,
-            default => <<<'HTML'
-<link rel="home" href="http://portfolio.test">
-<link rel="canonical" href="http://portfolio.test/{$locale}">
-<meta name="title" content="Modern, secure Websites and Services - festor.info">
-<meta name="keywords" content="Fullstack,Freelance,Webdev,Sicher,Modern,Schnell,PHP,Golang,Typescript,Vue,Deutsch,Englisch,Japanisch">
-<meta name="description" content="Modern, secure websites and services. I realize your dream project quickly and reliably. Good communication is important to me. - festor.info">
-<meta name="twitter:site" content="@Denakino">
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:creator" content="@Denakino">
-<meta name="twitter:description" content="Modern, secure websites and services. I realize your dream project quickly and reliably. Good communication is important to me. - festor.info">
-<meta name="twitter:image" content="{$imagePath}">
-<meta name="twitter:title" content="Modern, secure Websites and Services - festor.info">
-<meta name="og:description" content="Modern, secure websites and services. I realize your dream project quickly and reliably. Good communication is important to me. - festor.info">
-<meta name="og:image" content="{$imagePath}">
-<meta name="og:locale" content="en_US">
-<meta name="og:site_name" content="festor.info">
-<meta name="og:title" content="Modern, secure Websites and Services - festor.info">
-<meta name="og:type" content="homepage">
-<meta name="og:url" content="http://portfolio.test/{$locale}">
-HTML,
-        };
+<meta name="og:title" content="{$titleShort}">
+<meta name="og:type" content="website">
+<meta name="og:url" content="{$path}/{$localeUrl}">
+HTML;
     }
 }
--- app/Livewire/Project/Index.php
+++ app/Livewire/Project/Index.php
@@ -33,6 +33,43 @@ class Index extends Component
         return view('livewire.project.index', [
             'locale' => $this->locale,
             'projects' => $projects,
+            'metatags' => $this->getMetatags(),
         ]);
     }
+
+    private function getMetatags(): string
+    {
+        /** @var string $path */
+        $path = config('app.url');
+        $imagePath = $path . public_path('img/danny-640.jpeg');
+
+        $title = __('metatags.title');
+        $titleShort = __('metatags.title.short');
+        $keywords = __('metatags.keywords');
+        $twitterHandle = __('metatags.twitter.username');
+        $locale = __('metatags.locale');
+        $localeUrl = __('metatags.locale.url');
+
+        return <<<HTML
+<link rel="home" href="{$path}">
+<link rel="canonical" href="{$path}/{$localeUrl}/projects">
+<meta name="title" content="{$title}">
+<meta name="keywords" content="{$keywords}">
+<meta name="description" content="{$title}">
+<meta name="twitter:site" content="{$twitterHandle}">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:creator" content="{$twitterHandle}">
+<meta name="twitter:description" content="{$title}">
+<meta name="twitter:image" content="{$imagePath}">
+<meta name="twitter:title" content="{$titleShort}">
+<meta name="og:description" content="{$title}">
+<meta name="og:image" content="{$imagePath}">
+<meta name="og:locale" content="{$locale}">
+<meta name="og:site_name" content="festor.info">
+<meta name="og:title" content="{$titleShort}">
+<meta name="og:type" content="website">
+<meta name="og:url" content="{$path}/{$localeUrl}/projects">
+HTML;
+    }
+
 }
