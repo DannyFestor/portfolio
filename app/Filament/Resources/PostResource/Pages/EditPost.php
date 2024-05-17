8517ded628c10fe5e5e8c7303893c717322cdcb5
Danny Festor <3449907+DannyFestor@users.noreply.github.com>
2024-05-18 08:08:27 +0900

    Add Show Post Button to Admin Panel (#11)

--- app/Filament/Resources/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/PostResource/Pages/EditPost.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
+use App\Models\Post;
 use Exception;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
@@ -19,6 +20,10 @@ class EditPost extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('view')
+                ->label('View Post')
+                ->url(fn (Post $record) => route('blog.show', $record))
+                ->openUrlInNewTab(),
             Actions\DeleteAction::make(),
         ];
     }
